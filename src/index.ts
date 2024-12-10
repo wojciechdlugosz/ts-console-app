f86748ac38fe6e5b447dd62a6b6f22e2b2f043e3
Wojciech Dlugosz <webmaster@styronet.pl>
2024-12-10 14:17:20 +0100

    Update StartApp

--- src/index.ts
+++ src/index.ts
@@ -23,19 +23,6 @@ enum Action {
     action: Action
   }
 
-const startApp = () => {
-  inquirer.prompt([{
-    name: 'action',
-    type: 'input',
-    message: 'How can I help you?',
-  }]).then(async (answers: { action: 'list' | 'add' | 'remove' | 'quit' }) => {
-    console.log("Chosen action: " + answers.action);
-    startApp();
-    if (answers.action === "quit")
-      return;
-  });
-}
-
 class Message {
     private content: string;
   
@@ -112,5 +99,57 @@ class UsersData {
     return user.age > 0 && user.name.length > 0;
   }
 }
+
+const users = new UsersData();
+
+console.log("\n");
+console.info("???? Welcome to the UsersApp!");
+console.log("====================================");
+Message.showColorized(MessageVariant.Info, "Available actions");
+console.log("\n");
+console.log("list – show all users");
+console.log("add – add new user to the list");
+console.log("remove – remove user from the list");
+console.log("quit – quit the app");
+console.log("\n");
+
+const startApp = () => {
+    inquirer.prompt([{
+      name: 'action',
+      type: 'input',
+      message: 'How can I help you?',
+    }]).then(async (answers: InquirerAnswers) => {
+      switch (answers.action) {
+        case Action.List:
+          users.showAll();
+          break;
+        case Action.Add:
+          const user = await inquirer.prompt([{
+            name: 'name',
+            type: 'input',
+            message: 'Enter name',
+          }, {
+            name: 'age',
+            type: 'number',
+            message: 'Enter age',
+          }]);
+          users.add(user);
+          break;
+        case Action.Remove:
+          const name = await inquirer.prompt([{
+            name: 'name',
+            type: 'input',
+            message: 'Enter name',
+          }]);
+          users.remove(name.name);
+          break;
+        case Action.Quit:
+          Message.showColorized(MessageVariant.Info, "Bye bye!");
+          return;
+      }
+  
+      startApp();
+    });
+  }
 
 startApp();
